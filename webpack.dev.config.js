b42d2c7215fdcc44956fa5740fd816d6985f362a
yuyuehui <m18758428901@163.com>
2022-03-02 23:24:10 +0800

    feat: 优化

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -24,6 +24,7 @@ module.exports = {
       '**/node_modules',
       '**/.idea',
       '**/.vscode',
+      '**/.git',
       `${srcPath}/**/*.js`,
       `${srcPath}/**/*.ts`,
       `${srcPath}/**/*.json`,
